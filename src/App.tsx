c25afbd37672fabf5e2c00112fdafacf22822b3e
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-28 16:12:12 +0400

    feat: navigate to /dashboard/sales always

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./theme";
 import { Sales } from "./pages/Dashboard/Sales";
-import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -11,7 +15,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/dashboard/sales" element={<Sales />} />
-          <Route path="*" element={<NotFound />} />
+          <Route
+            path="*"
+            element={<Navigate to="/dashboard/sales" replace />}
+          />
         </Routes>
       </Router>
     </ThemeProvider>
